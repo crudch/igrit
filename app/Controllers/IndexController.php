24d7b8bc79da3b31a4599b5194d696de68e33fc3
Skip <skiphog@mail.ru>
2019-01-03 13:03:08 +0300

    ~ registry

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Models\Users\AuthUser;
 use Crudch\Foundation\Controller;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        var_dump(
-           auth()
-        );
+        return view('index', [
+            'username' => 'Vasya',
+        ]);
     }
 }
