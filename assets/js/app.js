d6fb16e1f6951d559ccbe2e3db33e842d4a2ac52
Skip <skiphog@mail.ru>
2019-01-09 17:34:13 +0300

    ~ npm modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
-//
+/*
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Vue = require('vue');*/
+
+document.querySelector('h1').classList.add('fuck');
