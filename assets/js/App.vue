5d364c3140d631fc13062a1366a168f2ec3df488
Skip <skiphog@mail.ru>
2019-01-12 11:54:37 +0300

    ~ Vue part #4 Slots

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="container mt-5">
+        <my-header>
+            <h1 class="text-center">SLOT</h1>
+            <h2 slot="subtitle">Sub Title</h2>
+        </my-header>
         <div class="row">
             <div class="col-sm">
                 <product-list></product-list>
@@ -12,11 +16,13 @@
 </template>
 
 <script>
-  import ProductList from './ProductList';
+  import myHeader from './Header';
   import DetailsView from './Details';
+  import ProductList from './ProductList';
 
   export default {
     components: {
+      myHeader,
       ProductList,
       DetailsView
     },
