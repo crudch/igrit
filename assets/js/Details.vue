5589f8c9f09f1e9866ed547561b2c1de876245f6
Skip <skiphog@mail.ru>
2019-01-14 11:47:39 +0300

    ~ Vue part #7 Filters

--- assets/js/Details.vue
+++ assets/js/Details.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card" v-fuck>
         <div class="card-header">
-            {{ product.title }}
+            {{ product.title | reverse }}
         </div>
         <div class="card-body">
             <p>{{ product.body}}</p>
@@ -26,6 +26,11 @@
         bind (el, binding, vnode) {
           setTimeout(() => el.style.boxShadow = '5px 7px 5px rgba(0,0,0,0.3)', 1000);
         }
+      }
+    },
+    filters: {
+      reverse(value) {
+        return value && value.split('').reverse().join('');
       }
     }
   };
