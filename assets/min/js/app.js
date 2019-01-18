8a47ab8e9d9a55fb12efcff09eb521453f01ddf1
Skip <skiphog@mail.ru>
2019-01-18 16:52:42 +0300

    ~ onOff

--- assets/min/js/app.js
+++ assets/min/js/app.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+
 UIkit.use(Icons);
 
 export const EventEmitter = new Vue();
