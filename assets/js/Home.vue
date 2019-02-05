e89127a40d269bb521a646d0e9ba508576b2e72b
Skip <skiphog@mail.ru>
2019-02-05 21:59:25 +0300

    ~ Home page lorem

--- assets/js/Home.vue
+++ assets/js/Home.vue
@@ -1,11 +1,23 @@
 <template>
     <main>
         <h1 class="title">Главная страничка</h1>
+        <div>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad consequatur cumque ea eligendi esse est eum illum incidunt laborum molestiae,
+                natus nulla odit praesentium quae repudiandae ullam velit, vero voluptatem!
+            </p>
+
+            <p>
+                Accusamus blanditiis corporis excepturi fugiat hic iste nostrum porro.
+                Accusamus assumenda consequuntur cum facere ipsam modi porro repellendus vel veniam!
+                Delectus eius error expedita hic illo odit officiis, perferendis sint!
+            </p>
+        </div>
     </main>
 </template>
 
 <script>
-  export default {
-
-  };
+  export default {};
 </script>
